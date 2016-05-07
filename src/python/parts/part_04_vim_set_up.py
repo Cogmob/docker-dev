921f4d96d11b6a55fdea7a6cb6a1d4ea9c041696
cogmob <luke.avery@live.co.uk>
2016-05-07 17:49:59 +0100

    copies solarized theme

--- src/python/parts/part_04_vim_set_up.py
+++ src/python/parts/part_04_vim_set_up.py
@@ -5,8 +5,9 @@ part = {
             'name': 'set up vim',
             'instructions': [
                 {'arguments': [
-                    'mkdir ~/.vim/bundle',
+                    'mkdir -p ~/.vim/bundle',
                     'cd ~/.vim/bundle',
+                    'cp ~/unix-setup/src/files-to-copy/solarized.vim ~/.vim/colors/solarized.vim',
                     'rm -rf vim-colors-solarized',
                     'rm -rf neobundle.vim',
                     'git clone git://github.com/altercation/vim-colors-solarized.git',
