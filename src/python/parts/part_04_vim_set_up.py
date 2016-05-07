1aa9fc3d4d3f5b35b442e9c0141ead8066925ebd
Cogbot <luke.avery@live.co.uk>
2016-05-07 17:42:50 +0000

    do not launch vim within script

--- src/python/parts/part_04_vim_set_up.py
+++ src/python/parts/part_04_vim_set_up.py
@@ -13,5 +13,4 @@ part = {
                     'git clone git://github.com/altercation/vim-colors-solarized.git',
                     'cd ~',
                     'mkdir -p /a/.vim/bundle',
-                    'git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim',
-                    'vim +NeoBundleInstall']}]}]}
+                    'git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim']}]}]}
