f3f1a66e368dd0bc71a034e7dfe1185d202e7251
Cogbot <luke.avery@live.co.uk>
2016-06-20 11:15:09 +0000

    do not start zsh during setup

--- src/python/parts/part_01_install_zsh.py
+++ src/python/parts/part_01_install_zsh.py
@@ -12,10 +12,6 @@ part = {
                 'git config --global user.name "cogmob"',
                 'git config --global user.email "luke.avery@live.co.uk"']}]},
         {
-            'name': 'install zsh',
-            'whitelist': 'none',
-            'instructions': [{'arguments': ['sudo apt-get zsh -f']}]},
-        {
             'name': 'set up oh my zsh',
 	    'instructions': [{'arguments': ['sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"']},
                 {'arguments': ['zsh']}]}]}
