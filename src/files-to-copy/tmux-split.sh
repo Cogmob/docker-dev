cc06eb0d779f22293a48ef5287a5772d03c59655
cogmob <luke.avery@live.co.uk>
2016-10-07 16:08:25 +0100

    added requirement for numbering new split ts

--- src/files-to-copy/tmux-split.sh
+++ src/files-to-copy/tmux-split.sh
@@ -2,11 +2,11 @@
 # create a new tmux pane with a vim split and rename it
 echo 'creating tmux split for project '$1
 tmux new-window -n $1
-tmux send-keys -t 1 "cd ~/"$1 Enter
-tmux send-keys -t 1 "tmux split -h" Enter
-tmux send-keys -t 1 "vim" Enter
-tmux send-keys -t 1 Space "t1j"
+tmux send-keys -t $2 "cd ~/"$1 Enter
+tmux send-keys -t $2 "tmux split -h" Enter
+tmux send-keys -t $2 "vim" Enter
+tmux send-keys -t $2 Space "t1j"
 sleep 4
-tmux send-keys -t 1 "cd ~/"$1 Enter
-tmux send-keys -t 1 "clear" Enter
+tmux send-keys -t $2 "cd ~/"$1 Enter
+tmux send-keys -t $2 "clear" Enter
 tmux select-pane -L
