83fd24021dfce3211297ab61b380b39c5713e2bf
Lambda Games <lambda.games.mail@gmail.com>
2017-05-05 16:42:06 +0100

    remove limit on prep to 40 items

--- src/other/prep.sh
+++ src/other/prep.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-grep -Iril --include=\*$1\* $2 --color=never --exclude-dir=\*{.git,node_modules} --exclude=\*.{anim,atf,bmp,bnk,csh,dds,exe,fla,jpg,mani,noise,png,psd,sdb,stationData,str,swf,sysmeta,tga,ttf,wem,xls,xmdl} | head -40 | python3 ~/unix_setup/src/other/populate_search_dir.py $3 --title $1_$2
+grep -Iril --include=\*$1\* $2 --color=never --exclude-dir=\*{.git,node_modules} --exclude=\*.{anim,atf,bmp,bnk,csh,dds,exe,fla,jpg,mani,noise,png,psd,sdb,stationData,str,swf,sysmeta,tga,ttf,wem,xls,xmdl} | python3 ~/unix_setup/src/other/populate_search_dir.py $3 --title $1_$2
