fd47f5aaf44a1bafc058965330f1283a3da481c9
Lambda Games <lambda.games.mail@gmail.com>
2017-07-17 14:27:34 +0100

    allow autocomplete for ts

--- src/other/tmux-split.sh
+++ src/other/tmux-split.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # create a new tmux pane with a vim split and rename it
-echo 'creating tmux split for project '$1
-tmux new-window -n $1
-tmux send-keys -t $2 "cd ~/"$1 Enter
+project_name=$(realpath $1* | awk -F"/" '{print $NF}')
+echo 'creating tmux split for project '$project_name
+tmux new-window -n $project_name
+tmux send-keys -t $2 "cd ~/"$project_name Enter
 tmux send-keys -t $2 "tmux split -h" Enter
 tmux send-keys -t $2 "vim" Enter
 tmux send-keys -t $2 Space "t1j"
 sleep 4
-tmux send-keys -t $2 "cd ~/"$1 Enter
+tmux send-keys -t $2 "cd ~/"$project_name Enter
 tmux send-keys -t $2 "clear" Enter
 tmux select-pane -L
+clear
