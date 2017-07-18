7a1031eab0628822c8ed4c2eaafc19a96c35603d
Lambda Games <lambda.games.mail@gmail.com>
2017-07-18 11:45:06 +0100

    do not print useless line of feedback after ts command

--- src/other/tmux-split.sh
+++ src/other/tmux-split.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 # create a new tmux pane with a vim split and rename it
 project_name=$(realpath $1* | awk -F"/" '{print $NF}')
-echo 'creating tmux split for project '$project_name
 tmux new-window -n$project_name "~/unix_setup/src/other/tmux-split-b.sh $project_name"
 tmux select-pane -L
-
-#sleep 3
-#tmux send-keys -t $2 "cd ~/"$project_name Enter
-#tmux send-keys -t $2 "clear" Enter
-#tmux send-keys -t $2 "vim" Enter
-#tmux send-keys -t $2 Space "t1j"
-#tmux split -h
-#sleep 3
-#tmux send-keys -t $2 "cd ~/"$project_name Enter
-#tmux send-keys -t $2 "clear" Enter
-#tmux select-pane -L
-#clear
