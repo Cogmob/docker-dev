681568e5b62d8b769ef4b91007c6f626fd154d34
Lambda Games <lambda.games.mail@gmail.com>
2017-09-14 17:57:24 +0100

    launch ahk properly

--- src/python/parts/part_05_auto_hotkey.py
+++ src/python/parts/part_05_auto_hotkey.py
@@ -16,4 +16,4 @@ def get_part(config):
 			    'git clone --depth 1 git@bitbucket.org:Cogbot/useful.git',
                             'mv useful .useful',
                             'ln -s -f ahk/main.ahk "/cygdrive/c/Users/%s/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/main.lnk"' % config['home_path'],
-			    '../.useful/ahk/ahk.exe ../ahk/main.ahk &']}]}]}
+			    '.useful/ahk/ahk.exe ../ahk/main.ahk &']}]}]}
