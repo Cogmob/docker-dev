5775a03809fa4b0451678e6320bc79e28ae3c36e
Lambda Games <lambda.games.mail@gmail.com>
2017-09-14 12:02:21 +0100

    do not install omz

--- src/python/parts/part_01_install_zsh.py
+++ src/python/parts/part_01_install_zsh.py
@@ -12,7 +12,4 @@ def get_part(config):
 		    'name': 'set up git user',
 		    'instructions': [{'arguments': [
 			'git config --global user.name "cogmob"',
-			'git config --global user.email "luke.avery@live.co.uk"']}]},
-		{
-		    'name': 'set up oh my zsh',
-		    'instructions': [{'arguments': ['sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"']}]}]}
+			'git config --global user.email "luke.avery@live.co.uk"']}]}]}
