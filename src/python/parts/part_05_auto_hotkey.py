6a88dd5f942edbb2506fa131d92a69e78dbb0bed
Lambda Games <lambda.games.mail@gmail.com>
2017-09-14 17:22:03 +0100

    shallow clones on init

--- src/python/parts/part_05_auto_hotkey.py
+++ src/python/parts/part_05_auto_hotkey.py
@@ -10,10 +10,9 @@ def get_part(config):
 			{'arguments': [
 			    'cd ~',
 			    'rm -rf ahk',
-			    'git clone git@bitbucket.org:Cogbot/ahk.git',
-			    'git clone git@bitbucket.org:Cogbot/first_principles.git',
-			    'git clone git@bitbucket.org:Cogbot/useful.git',
-			    'git clone git@bitbucket.org:Cogbot/cv.git',
-			    'git clone git@gitlab.org:lambdagames/useful.git',
+			    'git clone --depth 1 git@bitbucket.org:Cogbot/ahk.git',
+			    'git clone --depth 1 git@bitbucket.org:Cogbot/first_principles.git',
+			    'git clone --depth 1 git@bitbucket.org:Cogbot/cv.git',
+			    'git clone --depth 1 git@gitlab.org:lambdagames/useful.git',
                             'ln -s -f ahk/main.ahk "/cygdrive/c/Users/%s/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/main.lnk"' % config['home_path'],
 			    'ahk/main.ahk']}]}]}
