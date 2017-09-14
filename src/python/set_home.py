3df2429ecda73863db6f5184c588d05ea824e5e2
Lambda Games <lambda.games.mail@gmail.com>
2017-09-14 12:46:30 +0100

    slight text change

--- src/python/set_home.py
+++ src/python/set_home.py
@@ -3,10 +3,7 @@ import subprocess as s
 def set_home(data):
     print ''
     print 'what is your windows username?'
-    print 'this is needed for setting the startup programs'
-    print ''
-    s.Popen(['realpath', '~'])
-    s.Popen(['echo'])
+    print '(this is needed for setting the startup programs)'
     print ''
     home_path = raw_input('> ')
     data['home_path'] = home_path
