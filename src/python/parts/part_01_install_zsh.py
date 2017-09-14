441677db91f27afa4ddcf792e3ecf35543ce9a3b
Lambda Games <lambda.games.mail@gmail.com>
2017-09-14 12:08:46 +0100

    move setting of home directory into readme so it happens before the install script is run

--- src/python/parts/part_01_install_zsh.py
+++ src/python/parts/part_01_install_zsh.py
@@ -3,12 +3,6 @@ def get_part(config):
 	    'name': 'set up zsh',
 	    'steps': [
 		{
-		    'name': 'set up non identifiable home directory',
-		    'instructions': [{'arguments': [
-			"export HOME=`cygpath -w -p /`",
-                        'mkpasswd -l -p "$(cygpath -H)" > /etc/passwd',
-                        "babun install"]}]},
-		{
 		    'name': 'set up git user',
 		    'instructions': [{'arguments': [
 			'git config --global user.name "cogmob"',
