1cc3f0db635dd1de6c39e7c23c638f65d723326b
Luke Avery <luke.avery@cambridgeconsultants.com>
2018-03-14 11:43:29 +0000

    swap prep and irep

--- src/other/irep.sh
+++ src/other/irep.sh
@@ -2,4 +2,4 @@
 if [[ $3 ]]; then
     cd $3
 fi
-grep -Irl --include=\*$1\* $2 --color=never --exclude-dir=\*{.git,node_modules} --exclude=\*.{anim,atf,bmp,bnk,csh,dds,exe,fla,jpg,mani,noise,png,psd,sdb,stationData,str,swf,sysmeta,tga,ttf,wem,xls,xmdl} | python3 ~/unix_setup/src/other/populate_search_dir.py $4 --title $1_$2
+grep -Iril --include=\*$1\* $2 --color=never --exclude-dir=\*{.git,node_modules} --exclude=\*.{anim,atf,bmp,bnk,csh,dds,exe,fla,jpg,mani,noise,png,psd,sdb,stationData,str,swf,sysmeta,tga,ttf,wem,xls,xmdl} | python3 ~/unix_setup/src/other/populate_search_dir.py $4 --title $1_$2
