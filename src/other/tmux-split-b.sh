fa29b77c9dce00584232ba49bd03085624934e79
Luke Avery <luke.avery@cambridgeconsultants.com>
2018-06-25 16:14:16 +0100

    renamed files-to-copy directory to rc-files

--- src/other/tmux-split-b.sh
+++ src/other/tmux-split-b.sh
@@ -6,13 +6,13 @@ fi
 
 tmux split-window -h "cd ~/$1 ; vim readme.md"
 
-tmux split-window -b -l 1 -t 0 "read"
-tmux split-window -l 1 -t 1 "read"
-tmux split-window -b -l 1 -t 3 "read"
-tmux split-window -l 1 -t 4 "read"
+tmux split-window -b -l 0 -t 0 "read"
+tmux split-window -l 0 -t 1 "read"
+tmux split-window -b -l 0 -t 3 "read"
+tmux split-window -l 0 -t 4 "read"
 
-tmux split-window -h -l 12 -t 1 "read"
-tmux split-window -bh -l 12 -t 1 "read"
-tmux split-window -h -l 12 -t 6 "read"
+tmux split-window -h -l 6 -t 1 "read"
+tmux split-window -bh -l 6 -t 1 "read"
+tmux split-window -h -l 6 -t 6 "read"
 
-tmux split-window -t 2 -l 1 "zsh -is eval \"cd ~/$1\"" ; tmux select-pane -t 2 ; zsh -is eval "cd ~/$1"
+tmux split-window -t 2 -l 0 "zsh -is eval \"cd ~/$1\"" ; tmux select-pane -t 2 ; zsh -is eval "cd ~/$1"
