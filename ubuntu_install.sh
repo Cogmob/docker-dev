d07270be4447afe5803875b67a4c5f2c6b52cb25
Luke Avery <luke.avery@cambridgeconsultants.com>
2018-08-31 16:25:29 +0100

    removed useless line from ubuntu install

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -25,7 +25,6 @@ rm -rf vim-colors-solarized
 rm -rf neobundle.vim
 git clone git://github.com/altercation/vim-colors-solarized.git
 cd ~
-mkdir -p /a/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 mkdir -p ~/.vim/bundle/vim/bundle/vim-snippets
 vim +NeoBundleInstall +qall
