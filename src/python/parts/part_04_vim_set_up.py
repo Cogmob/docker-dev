c46d4e1ebb13defe64ff2a94e5f9e70c1d8b762a
Luke Avery <luke.avery@live.co.uk>
2019-02-10 12:18:55 +0000

    part_04_vim_set_up.py edited online with Bitbucket

--- src/python/parts/part_04_vim_set_up.py
+++ src/python/parts/part_04_vim_set_up.py
@@ -6,17 +6,13 @@ def get_part(config):
 		    'name': 'set up vim',
 		    'instructions': [
 			{'arguments': [
-			    'mkdir -p ~/.vimundo',
-			    'mkdir -p ~/.vim/bundle',
-			    'cd ~/.vim/bundle',
-			    'mkdir -p ~/.vim/colors',
-			    'ln -sf ~/unix_setup/src/other/solarized.vim ~/.vim/colors/solarized.vim',
-			    'rm -rf vim-colors-solarized',
-			    'rm -rf neobundle.vim',
-			    'git clone git://github.com/altercation/vim-colors-solarized.git',
-			    'cd ~',
-			    'mkdir -p /a/.vim/bundle',
-			    'git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim',
-			    'mkdir -p ~/.vim/bundle/vim/bundle/vim-snippets',
-			    'vim +NeoBundleInstall +qall',
-			    'ln -s ~/unix_setup/src/ulti-snips ~/.vim/bundle/vim/bundle/vim-snippets/UserUltiSnips']}]}]}
+				'mkdir -p ~/.vimundo',
+				'mkdir -p ~/.vim/bundle',
+				'cd ~/.vim/bundle',
+				'mkdir -p ~/.vim/colors',
+				'cd ~',
+				'curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh',
+				'sh installer.sh ~/.cache/dein',
+				'rm installer.sh',
+				'mkdir -p ~/.vim/bundle/vim/bundle/vim-snippets',
+				'vim +"call dein#install()" +qall']}]}]}
