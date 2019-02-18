4163b52f51b7d3e1f90e7f29f8b6ac985bc6e4e0
cogmob <luke.avery@live.co.uk>
2019-02-18 17:19:22 +0000

    modified how to keep a remote folder in sync

--- ssh/rsync_down.sh
+++ ssh/rsync_down.sh
@@ -1,2 +1,2 @@
 echo bringing changes from remote to local
-rsync -rav -e ssh --exclude='.git' --filter=':- .gitignore' cogmob@$(./get_ip_address.sh $1):/home/cogmob/dashboard/ ~/dashboard --delete
+rsync -rav -e ssh --exclude='.git' --filter=':- .gitignore' cogmob@$(./get_ip_address.sh $1):$(./get_remote_path.sh $1)/ $(./get_local_path.sh) --delete
--- ssh/rsync_up.sh
+++ ssh/rsync_up.sh
@@ -1,2 +1,2 @@
 echo sending changes from local to remote
-rsync -rav -e ssh --exclude='.git' --filter=':- .gitignore' ~/dashboard/ cogmob@$(./get_ip_address.sh $1):/home/cogmob/dashboard
+rsync -rav -e ssh --exclude='.git' --filter=':- .gitignore' $(./get_local_path.sh $1)/ cogmob@$(./get_ip_address.sh $1):$(./get_remote_path.sh $1)
